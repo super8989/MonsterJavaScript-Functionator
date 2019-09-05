3b6f88940741722c969ca5e282daa838d73327a9
Sam <super8989@gmail.com>
2019-09-04 23:23:10 -0400

    adding styles using JavaScript

--- script.js
+++ script.js
@@ -5,5 +5,16 @@
 
      myBlock = document.createElement("div");
      myBlock.textContent = "hello world";
+     myBlock.style.width = "100px";
+     myBlock.style.height = "100px";
+     myBlock.style.backgroundColor = "red";
+     myBlock.style.color = "white";
+
+     myBlock.style.lineHeight = "100px";
+     myBlock.style.textAlign = "center";
+     myBlock.style.position = "absolute";
+     myBlock.style.top = "100px";
+     myBlock.style.left = "150px";
+
      document.body.appendChild(myBlock);
  })
